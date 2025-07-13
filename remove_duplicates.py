426544f70048cec586d307df18caa44d9af85a6c
nevillemthw <nevillemathew2014@gmail.com>
2025-07-13 14:59:28 -0500

    Remove duplicates problem

---
+++ remove_duplicates.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> int:
+
+        # Basically takes all the elements from the list, sorts it and converts to a set
+        # By default set() does not keep duplicates
+        nums[:] = sorted(set(nums))
+
+        print(nums)
+        return print(int(len(nums)))
+
+solution = Solution()
+solution.removeDuplicates([2,10,10,30,30,30])
