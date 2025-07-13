80aad462d36292b1d92a7e9207776e4d028329a2
nevillemthw <nevillemathew2014@gmail.com>
2025-07-13 15:01:53 -0500

    Remove element problem

---
+++ remove_element.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def remove_element(nums: List[int], val: int) -> int:
+        # Removing from an array is a O(n) operation
+        # To modify the array in-place, not create a new array
+
+        k = 0
+        # Iterate over all the indexes instead of the values in the list
+        for i in reversed(range(len(nums))):
+            # If nums contains val, remove/pop
+            if nums[i] == val:
+                nums.pop(i)
+            k += 1
+
+
+        return k
+
+solution = Solution()
+solution.remove_element([0, 1, 2, 2, 3, 0, 4, 2], 2)
