9ec1880a5487563b072a8eb24e4fa782e54728b9
nevillemthw <nevillemathew2014@gmail.com>
2025-07-17 14:08:28 -0500

    Syntax error fix

---
+++ concatenate_array.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        # New array should be 2 * nums,basically concatenated of two nums arrays
+
+        ans = nums * 2
+
+        return ans
