e21e96875802808eb02fe57ae4af8439eaa60eee
nevillemthw <nevillemathew2014@gmail.com>
2025-07-21 23:28:13 -0500

    Not the optimal solution for concatenate_array.py

--- concatenate_array.py
+++ concatenate_array.py
@@ -2,6 +2,7 @@ from typing import List
 
 
 class Solution:
+    # Not optimal since not using iteration, one or two pass method
     def getConcatenation(self, nums: List[int]) -> List[int]:
         # New array should be 2 * nums,basically concatenated of two nums arrays
 
